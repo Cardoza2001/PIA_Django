c835f20092e32b0e41662a61d96c3570699ddb70
Stephanierdz <fanyrodriguez2112@gmail.com>
2025-05-18 10:20:00 -0600

    urls

--- PIA_Muebles_De_Oficina/PIA_Muebles_De_Oficina/urls.py
+++ PIA_Muebles_De_Oficina/PIA_Muebles_De_Oficina/urls.py
@@ -1,22 +1,13 @@
-"""
-URL configuration for PIA_Muebles_De_Oficina project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse  # 👈 Agrega esto
 
+# 👇 Define una vista básica
+def inicio(request):
+    return HttpResponse("Sitio de muebles de oficina!")
+
+# 👇 Agrega la nueva vista a las rutas
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', inicio),  # 👈 Esta línea es clave
 ]
